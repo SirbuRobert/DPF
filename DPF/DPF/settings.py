c22c89a14f7afef3889da1dfdf737ff0fd5cbaef
Boeriu George Cosmin <gboeriu@adobe.com>
2025-11-09 01:55:29 +0200

    HTML Files added

--- DPF/DPF/settings.py
+++ DPF/DPF/settings.py
@@ -128,7 +128,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+TEMPLATES[0]["DIRS"] = [BASE_DIR / "templates"]
+STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
@@ -150,3 +152,5 @@ MIDDLEWARE = [
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
+
+
